5540346b327203f411887e60a9c6691c48c064fa
Sami6399 <157350018+Sami6399@users.noreply.github.com>
2024-05-08 11:19:22 +0000

    conversion of Datatypes

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,33 @@
+let score = "sameer"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1; false => 0
+
+
+let isLoggedIn = "sameer"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+// 1 = > true; 0 => false
+// "" => false
+// "sameer" => true
+
+
+
+
+let someNumber = 12
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);

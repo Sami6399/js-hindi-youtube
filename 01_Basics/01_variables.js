7705538a65ceff516c8e998c7a5a5eba20083720
Sami6399 <157350018+Sami6399@users.noreply.github.com>
2024-05-08 07:32:05 +0000

    datatypes

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,24 @@
+const areaWise = "just"
+let stateWise = "kashmir"
+var countryWise = "india"
+localArea = "baramulla"
+let stateAction;
+
+
+
+// areaWise = "12231"  // not allowed
+//  console.log(areaWise); 
+
+stateWise = "jammu"
+countryWise = "russia"
+localArea = "sopore"
+
+
+
+// Prefer not use var
+// because of issue in block scope "{}" and functional scope
+
+console.log(stateWise);
+console.table([areaWise, stateWise, countryWise, localArea, stateAction]); // to execute multiple variables at a time
+
+
---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,31 @@
+"use strict"; // treat all js codes as newer version
+
+// alert (3+3)  // we are using nodejs, not browser
+
+console.log(3 
+    +
+    3);  // code readability should be high
+
+
+
+ let name = "sameer" // string datatype
+ let age = 25 // number datatype
+ let isLoggedIn = false // boolean datatype
+
+
+//  Primitive data types
+ 
+// number => 2 to power 53
+ // bigint => (for big numbers)
+ // string => "" , ''
+ // boolean => true/false
+ // null => standalone value e.g let state = null
+ // undefined => no value assingned
+ // symbol => used for uniqueness
+
+
+
+ // object
+
+ console.log(typeof undefined )// output = undefined
+ console.log(typeof null ) // output = object
